a7cdcef46d3b4d7add9e2b1cfa4b2bdecc92d91c
Gabriella Larangeira <glarangeirag@gmail.com>
2020-09-19 19:34:35 -0300

    Update app.js

--- idadeEmSegundos/app.js
+++ idadeEmSegundos/app.js
@@ -1,1 +1,7 @@
-alert("Hello world!")
+let age = parseInt(prompt("Qual é a sua idade?"))
+
+let ageInSeconds = age*365*24*60*60
+
+console.log(ageInSeconds)
+
+document.write ("Sua idade em segundos é igual a "+ageInSeconds+".")
